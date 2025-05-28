d48860cad5f6a656948374b53752f7b2ab09daa5
Arseniy Knyazev <poseaydone@ya.ru>
2025-05-28 16:44:15 +0500

    fix: added icons

--- src/app/(landing)/_components/WhyUs.tsx
+++ src/app/(landing)/_components/WhyUs.tsx
@@ -1,9 +1,12 @@
 import Section from "@/components/ui/section";
+import CheckmarkedDocument from "@/modules/common/icons/checkmarked-document";
+import HandWithHeart from "@/modules/common/icons/hand-with-heart";
+import TwoPeopleTalking from "@/modules/common/icons/two-people-talking";
 
 export default function WhyUs() {
 	return (
 		<Section
-			className="bg-primary text-on-primary py-[150px]"
+			className="bg-primary text-on-primary py-[150px] "
 			id="why-us"
 			title={
 				<>
@@ -13,9 +16,9 @@ export default function WhyUs() {
 				</>
 			}
 		>
-			<div className="flex flex-wrap justify-center gap-40 mx-auto">
-				<div className="flex flex-col items-center">
-					<div className="w-[115px] h-[115px] bg-secondary rounded-full mb-10" />
+			<div className="flex flex-wrap justify-center mx-auto w-full gap-10">
+				<div className="flex flex-col items-center flex-1 min-w-[200px]">
+					<HandWithHeart size={140} className="mb-10" />
 					<h3 className="text-2xl font-medium mb-4">Забота</h3>
 					<p className="text-center">
 						Поможем организовать
@@ -24,8 +27,8 @@ export default function WhyUs() {
 						<br />и вашим пожеланиям.
 					</p>
 				</div>
-				<div className="flex flex-col items-center">
-					<div className="w-[115px] h-[115px] bg-secondary rounded-full mb-10" />
+				<div className="flex flex-col items-center flex-1 min-w-[200px]">
+					<CheckmarkedDocument size={140} className="mb-10 -ml-5" />
 					<h3 className="text-2xl font-medium mb-4">Уверенность</h3>
 					<p className="text-center">
 						Гарантируем юридическую
@@ -35,8 +38,8 @@ export default function WhyUs() {
 						всех формальностей
 					</p>
 				</div>
-				<div className="flex flex-col items-center">
-					<div className="w-[115px] h-[115px] bg-secondary rounded-full mb-10" />
+				<div className="flex flex-col items-center flex-1 min-w-[200px]">
+					<TwoPeopleTalking size={140} className="mb-10" />
 					<h3 className="text-2xl font-medium mb-4">Понимание</h3>
 					<p className="text-center">
 						Предложим оптимальные
