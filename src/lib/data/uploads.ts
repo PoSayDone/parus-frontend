2d7715b268829d3dbbc94de0fb9eb4e6b9e903a1
PoSayDone <poseaydone@ya.ru>
2025-09-18 12:26:35 +0500

    fix: dockerfile

--- src/lib/data/uploads.ts
+++ src/lib/data/uploads.ts
@@ -13,6 +13,8 @@ export const uploadFile = async (file: File): Promise<string> => {
 		const fileExtension = file.name.split(".").pop();
 		const uniqueFileName = `${uuidv4()}.${fileExtension}`;
 
+		console.log(process.env);
+
 		if (process.env.S3_BUCKET) {
 			return await uploadToS3(buffer, uniqueFileName, file.type);
 		} else {
