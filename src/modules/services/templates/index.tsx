f9a36bf054329095ebe0acf2cd297d41e298d426
PoSayDone <poseaydone@ya.ru>
2025-11-09 21:31:52 +0500

    fix: removed useless comment

--- src/modules/services/templates/index.tsx
+++ src/modules/services/templates/index.tsx
@@ -16,7 +16,6 @@ export default function ServicesTemplate() {
 					</p>
 				</div>
 
-				{/* Services Grid */}
 				<ServicesList className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mb-8" />
 
 				<ContactSection
