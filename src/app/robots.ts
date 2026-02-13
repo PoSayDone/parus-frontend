3413bc1aaf22cfe7c3eef0bf064a56c2a3549989
Tatiana Gordeeva <tatianka.gordeeva@gmail.com>
2026-02-13 21:56:54 +0500

    fix: update robots.txt to separate Yandex and Googlebot

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,14 +1,24 @@
 import type { MetadataRoute } from "next";
 
-const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "https://parusritual.ru";
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "https://parus-ritual.ru";
 
 export default function robots(): MetadataRoute.Robots {
 	return {
-		rules: {
-			userAgent: "*",
-			allow: "/",
-			disallow: "/admin",
-		},
+		rules: [
+			{
+				userAgent: "*",
+				allow: "/",
+				disallow: "/admin",
+			},
+			{
+				userAgent: "Yandex",
+				disallow: "/admin",
+			},
+			{
+				userAgent: "Googlebot",
+				disallow: "/admin",
+			},
+		],
 		sitemap: `${baseUrl}/sitemap.xml`,
 	};
 }
