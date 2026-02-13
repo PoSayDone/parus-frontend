3e9427d06a615286e40b276f60550d8646a939a7
PoSayDone <poseaydone@ya.ru>
2026-02-13 18:20:23 +0500

    fix: phone number

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,5 +2,12 @@ import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs));
+}
+
+export function formatPhoneNumber(phoneNumber: string): string {
+  return phoneNumber.replace(
+    /(\d{1})(\d{3})(\d{3})(\d{2})(\d{2})/,
+    "$1 $2 $3-$4-$5",
+  );
 }
